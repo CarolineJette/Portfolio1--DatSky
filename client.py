b9ac787f1246a2a0fdd26c9be7ae792d293d2a2c
Caroline Jetteberg <s313564@oslomet.no>
2021-03-12 16:48:47 +0100

    Prøver meg litt frem med eksempler fra oppgaven

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import random
 
 print("Dette er en klient")
 
@@ -6,10 +7,16 @@ sock = socket.socket(type = socket.SOCK_DGRAM)
 #sock.connect(("localhost", 4242))
 
 while True:
-    print("Enter message: ", end = "")
-    msg = input()
-    print("Sending \"{}\" ".format(msg))
-    sock.sendto(msg.encode(), ("localhost", 4242))
-    reply = sock.recv(1024).decode()
-    print("Server replied: {}".format(reply))
+    #print("Enter message: ", end = "")
+    #msg = input()
+    #print("Sending \"{}\" ".format(msg))
+    
+    #reply = sock.recv(1024).decode()
+    #print("Server replied: {}".format(reply))
+    def alice(a, b = None):
+        return "I think {} sounds great!".format(a + "ing")
+
+    action = random.choice(["work", "play", "eat", "cry", "sleep", "fight"])
+    reply = "Alice: {}".format(alice(action))
+    sock.sendto(reply.encode(), ("localhost", 4242))
 
