0f0fe21789a272eacb19ee8c40ee6075bc936c09
Caroline Jetteberg <s313564@oslomet.no>
2021-03-14 23:06:12 +0100

    Mulig kommet litt videre, hentet litt inspo fra G4G

--- client.py
+++ client.py
@@ -1,22 +1,47 @@
 import socket
 import random
+import select
+import sys
 
 print("Dette er en klient")
 
-sock = socket.socket(type = socket.SOCK_DGRAM)
-#sock.connect(("localhost", 4242))
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+if len(sys.argv) != 3:
+    print("Correct usage: script, IP adress, port number, bot")
+    exit()
+
+ip = str(sys.argv[1])
+port = int(sys.argv[2])
+
+sock.connect((ip, port))
 
 while True:
+    socket_list = [sys.stdin, sock]
+    read_sockets, write_sockets, error_socket = select.select(socket_list, [], [])
+
+    for sockets in read_sockets:
+        msg = sockets.recv(1024).decode()
+        print(msg)
+
+sock.close()
+#sock.sendall(b"Hello world")
+#data = sock.recv(1024)
+
+#while True:
     #print("Enter message: ", end = "")
     #msg = input()
     #print("Sending \"{}\" ".format(msg))
     
     #reply = sock.recv(1024).decode()
     #print("Server replied: {}".format(reply))
-    def alice(a, b = None):
-        return "I think {} sounds great!".format(a + "ing")
+    #data, (addr, port) = sock.recvfrom(1024)
+    #def alice(a, b = None):
+    #    return "I think {} sounds great!".format(a + "ing")
 
-    action = random.choice(["work", "play", "eat", "cry", "sleep", "fight"])
-    reply = "Alice: {}".format(alice(action))
-    sock.sendto(reply.encode(), ("localhost", 4242))
+    #action = random.choice(["work", "play", "eat", "cry", "sleep", "fight"])
+    #data, (addr, port) = sock.recvfrom(1024)
+    #action = sock.recvfrom(1024).decode()
+    #reply = "Alice: {}".format(alice(action))
+    #sock.sendto(reply.encode(), ("localhost", 4242))
 
--- server.py
+++ server.py
@@ -1,16 +1,77 @@
 import socket
+import random
+import sys
+import _thread
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+if len(sys.argv)!=2:
+    print("Correct usage: script, port number")
+
+Port = int(sys.argv[1])
 
 print("Dette er serveren")
+sock.bind(("0.0.0.0", Port))
+sock.listen()
+#conn, addr = sock.accept()
 
-sock = socket.socket(type = socket.SOCK_DGRAM)
+list_of_clients = []
 
-sock.bind(("0.0.0.0", 4242))
+def clientthread(conn, addr):
+    action = random.choice(["sing", "drink", "clean", "eat", "sleep", "study", "think", "work"])
+    msg = "Host: Can we {}, please? ".format(action)
+    print(msg)
+    conn.sendall(msg.encode())
+
+    while True:
+        try:
+            answer = conn.recv(1024)
+            if answer:
+                print("<" + addr[0] + "> " + answer)
+                answer_to_send = "<" + addr[0] + "> " + answer
+                broadcast(answer_to_send, conn)
+            else:
+                remove(conn)
+        except:
+            continue
+
+def broadcast(msg, conn):
+    for client in list_of_clients:
+        if client!=conn:
+            try:
+                client.send(msg)
+            except:
+                client.close()
+                remove(client)
+
+def removeconn(conn):
+    if conn in list_of_clients:
+        list_of_clients.remove(conn)
 
 while True:
-    print("Awaiting message...")
-    data, (addr, port) = sock.recvfrom(1024)
-    print("Message from {}:{}\nContent: {}".format(addr, port, data.decode()))
-    print("Enter reply: ", end = "")
-    reply=input()
-    sock.sendto(reply.encode(), (addr, port))
+    conn, addr = sock.accept()
+    list_of_clients.append(conn)
+    print(addr[0] + " connected")
 
+    _thread.start_new_thread(clientthread, (conn, addr))
+
+conn.close()
+sock.close()
+
+#with conn: 
+    #print("Connected by ", addr)
+    #while True:
+        #data = conn.recv(1024)
+        #if not data:
+         #   break
+
+        #conn.sendall(data)
+        #print("Awaiting message...")
+        
+        #print("Message from {}:{}\nContent: {}".format(addr, port, data.decode()))
+        #action = random.choice(["sing", "drink", "clean", "eat", "sleep", "study", "think", "work"])
+        #msg = "Host: Can we {}, please? ".format(action)
+        #print(msg)
+        #data, (addr, port) = conn.recvfrom(1024)
+        #conn.sendto(action.encode(), (addr, port))
+        #print(addr, port, data.decode())
