a36d8bcccb2df1e8ca3e7ae2cd671b1eacce5f78
Caroline Jetteberg <s313564@oslomet.no>
2021-03-17 23:44:21 +0100

    Lagt inn noen kommentarer og fiksa på litt småting

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import sys
 import random
 import time
 
+#The client program takes in 3 command line parameters in addition to the script
 if len(sys.argv) != 4:
     print("Correct usage: script, IP adress, port number, bot")
     exit()
@@ -11,13 +12,16 @@ IP = str(sys.argv[1])
 PORT = int(sys.argv[2])
 bot = str(sys.argv[3])
 
+#Creates a TCP socket and connects to the ip address and port given as parameters
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP, PORT))
 
+#Lists with good/bad/random actions
 good_things = ["party", "drink", "scream", "eat", "catch", "sing", "sleep", "think"]
 bad_things = ["cry", "steal", "beat", "clean", "study", "work"]
-random_action = random.choice(["die", "paint a wall", "wash my clothes", "kick a ball"])
+random_action = random.choice(["die", "paint a wall green", "wash our clothes", "kick a ball", "buy some popcorn"])
 
+#Bot functions
 def amy(a):
     if a in bad_things:
         return "Amy: I don't think {} is such a great idea... Can't we do anything else, such as {}?".format(a + "ing", random.choice(good_things))
@@ -27,7 +31,7 @@ def jake(a, b = None):
     if a in bad_things:
         return "Jake: Omg, {} is the beeest!".format(a + "ing")
     else:
-        return "Jake: Omg, {} is laaame, let's go {} instead".format(a + "ing", b + "ing")
+        return "Jake: Omg, {} is laaame, let's go {} instead".format(a + "ing", b)
 def roza(a, b):
     return "Roza: I will rather {} than {}, but whatever...".format(b, a)
 def holt(a):
@@ -42,6 +46,7 @@ while True:
     else:
         print(host_msg)
 
+    #Trying to get the action from the host message
     try:
         split_msg = host_msg.split()
         action = split_msg[6]
@@ -50,6 +55,7 @@ while True:
 
     random_action = random.choice(["sleep", "paint a wall", "wash my clothes", "kick a ball"])
 
+    #Calls the bot functions dependent on the bot parameter
     if bot == 'Amy':
         message = "{}".format(amy(action))
     elif bot == 'Jake':
@@ -59,7 +65,7 @@ while True:
     elif bot == 'Holt':
         message = "{}".format(holt(action))
     else:
-        message = "Host: {} the Bot isn't here today".format(bot)
+        message = "Host: {} the Bot isn't with us today".format(bot)
 
     client_socket.send(message.encode())
     print(message)
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import random
 
+#The server program takes in 1 command line parameter, port number, in addition to the script
 if len(sys.argv)!=2:
     print("Correct usage: script, port number")
     exit()
@@ -9,23 +10,29 @@ if len(sys.argv)!=2:
 IP = "127.0.0.1"
 Port = int(sys.argv[1])
 
+#Creates a TCP socket and bind to localhost (127.0.0.1) and the port given as parameter
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((IP, Port))
 server_socket.listen()
 
 connected = []
+
 stop_while = False
+
 while not stop_while:
+    #Accepts as many incoming connections the user wants
     client_socket, client_address = server_socket.accept()
     connected.append(client_socket)
     message = input("Do you want to connect more clients, y/n? ")
     if message == 'n':
         stop_while = True
 
+#The message from host/server that the bots will be to replying to
 action = random.choice(["sing", "drink", "clean", "eat", "sleep", "study", "think", "work", "party", "scream", "steal", "beat", "catch", "cry"])
 host_msg = "Host: Do you guys want to {} today? ".format(action)
 print(host_msg)
 
+#Runs through the connections/clients and sends/receives the bots'replies
 for i in connected:
     i.send(host_msg.encode())
 for i in connected:
